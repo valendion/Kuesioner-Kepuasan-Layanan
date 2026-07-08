3a1bad47d162a3f34d5cc8659575ab9d238bf028
valendion <valendion874@gmail.com>
2026-07-08 18:30:07 +0800

    fix(add import library in api satisfaction)

--- app/api/satisfaction/route.js
+++ app/api/satisfaction/route.js
@@ -1,28 +1,9 @@
+import { prisma } from "@/app/lib/prisma/initialization";
+import { NextResponse } from "next/server";
+
 export const POST = async (request) => {
   try {
     const body = await request.json();
-
-    // Validasi required fields
-    const required = [
-      "age",
-      "gender",
-      "education",
-      "occupation",
-      "services_received",
-      "answers",
-    ];
-    for (const field of required) {
-      if (
-        body[field] === undefined ||
-        body[field] === null ||
-        body[field] === ""
-      ) {
-        return NextResponse.json(
-          { message: `Field '${field}' wajib diisi` },
-          { status: 400 },
-        );
-      }
-    }
 
     const patient = await prisma.patient.create({
       data: {
@@ -32,13 +13,13 @@ export const POST = async (request) => {
         occupation: body.occupation,
         services_received: body.services_received,
         answers: JSON.stringify(body.answers),
-        suggestion: body.suggestion || null, // handle undefined
+        suggestion: body.suggestion || null,
       },
     });
 
     return NextResponse.json(patient, { status: 200 });
   } catch (error) {
-    console.error("API Error:", error);
+    console.error("API ERROR:", error);
     return NextResponse.json({ message: error.message }, { status: 500 });
   }
 };
