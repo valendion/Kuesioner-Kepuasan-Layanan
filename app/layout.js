87d030357b8651f3530bd7cc78cb168978bb4ec3
valendion <valendion874@gmail.com>
2026-07-14 20:29:03 +0800

    fix(change mata data)

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import { SWRProvider } from "./swr/swr-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Selamat Datang",
+  title: "Kuesioner Kepuasan Layanan",
 };
 
 export default function RootLayout({ children }) {
